087be5438455df8aeae4b39f37725799c5d517fd
Werner Mairl <werner.mairl@gmail.com>
2016-11-12 22:02:51 +0100

    lot of refactorings specially for PATH variable writes not done but big part is working!

--- nvm/public/Set-NodeVersion.ps1
+++ nvm/public/Set-NodeVersion.ps1
@@ -24,17 +24,33 @@
        Set-NodeVersion v5.0.1 -Persist Machine
        Set and persist in permamant system path for the machine (Note: requires an admin shell)
     #>
+
+    [cmdletbinding(DefaultParameterSetName="Latest")]
     param(
-        [string]
-        [Parameter(Mandatory=$false)]
-        [ValidatePattern('^v\d(\.\d{1,2}){0,2}$')]
-        $Version,
-        [string]
-        [ValidateSet('User', 'Machine')]
-        [Parameter(Mandatory=$false)]
-        $Persist
+         [Parameter(Mandatory=$false, ParameterSetName="Latest")][Switch]$Latest=$true
+        ,[Parameter(Mandatory=$false, ParameterSetName="Version")][ValidateScript({Validate-Version -Version $_})][string]$Version
+        ,[Parameter(Mandatory=$false, ParameterSetName="Fuzzy")][Switch]$Fuzzy=$false
+        ,[ValidateSet("Process", "User", "Machine")][Parameter(Mandatory=$false)][string]$Persist = "Process" #current process is the default!
+        ,[Parameter(Mandatory=$false)][Switch]$Force=$false #ensure re-activate with the provided value for $Persist argument 
     )
 
+    #Changes by WM 11/2016
+    #added ParameterSets and cmdletbinding
+    #added "Never" to Parameter "Persist" => Q: the SetEnvironmentVariable command uses User/Machine/Process, should/can we do the same ?
+    #strings should NEVER be written direct to the pipeline => they goes to the host! Use Write-Verbose (et.al) for that
+    #deactivate/detect current version seems NOT to be coded...first try!
+    #access and calculation of environmentvariable PATH rewritten/refactored!
+
+
+    [System.Version]$VersionToUse = $null;
+    if ($PSCmdlet.ParameterSetName -eq "Version")
+    {
+        $VersionToUse = Validate-Version -Version $Version -Passthrough;
+    }
+
+
+    #fuzzy matching causes a lot of risk and complexity, specially on the api-surface. We should remove that or at least remove the feature from the Parameter "Version" and use another Parameter (Set) for this!
+    <#
     if ([string]::IsNullOrEmpty($Version)) {
         if (Test-Path .\.nvmrc) {
             $VersionToUse = Get-Content .\.nvmrc -Raw
@@ -49,7 +65,8 @@
     }
 
     $VersionToUse = $VersionToUse.replace("`n","").replace("`r","")
-
+    #>
+    <#
     if (!($VersionToUse -match "v\d\.\d{1,2}\.\d{1,2}")) {
         "Version found is not a full version, using fuzzy matching"
         $VersionToUse = Get-NodeVersions -Filter $VersionToUse | Select-Object -First 1
@@ -59,33 +76,61 @@
             return
         }
     }
+    #>
+    if ($VersionToUse -eq $null)
+    {
+      throw "VersionToUse not calculated! Something with the parameters goes wrong or not implemented ParameterSet $($PSCmdlet.ParameterSetName)."
+    }
 
-    $nvmwPath = Get-NodeInstallLocation
+    $npmCommand=Get-Command -Name "npm" -ErrorAction Ignore; #it returns not null if some npm is installed/available!
+    $nodeCommand=Get-Command -Name "node.exe" -ErrorAction Ignore;
 
-    $requestedVersion = Join-Path $nvmwPath $VersionToUse
+    if ($Force.IsPresent -eq $false)
+    {
+        if (($npmCommand -ne $Null) -and ($nodeCommand -ne $null))
+        {
+          #some version is active, we must find out if it is the requested!
+          [int]$compare = Compare-Version $nodeCommand.Version $VersionToUse;
+          if ($compare -eq 0)
+          {
+            #PROBLEM/RISK: a version is installed BUT Parameter "Persist" can have different value. not sure if we can check that here!
+            Write-Verbose "The requested version $($VersionToUse.ToString()) is currently active in folder $($nodeCommand.Source)";
+            return;
+          }
+        }
+    }
+    else
+    {
+      Write-Verbose "-Force is selected";
+    }
 
-    if (!(Test-Path -Path $requestedVersion)) {
-        "Could not find node version $VersionToUse"
+    $nvmwPath = Get-NodeInstallLocation -Resolve:$true; #Resolve ensures a FULL Path name (we need that for env:Path) and it throws a exception if the path not exists => 0 installed versions => exception OK!
+    [string]$foldername = $VersionToUse.ToString();
+    $installFolderPath = Join-Path $nvmwPath $foldername;
+    [bool]$folderExists=Test-Path -Path $installFolderPath;
+
+    if ($folderExists -eq $false)
+    {
+        # error management/exception !?
+        Write-Verbose "Could not find node version $($VersionToUse.ToString())";
         return
     }
 
-    # immediately add to the current powershell session path
-    $env:Path = "$requestedVersion;$env:Path"
+    #depending on the Persist-Level we must write the PATH variable multiple times!
+    #1. for the current process: ALWAYS 
+    Update-EnvironmentPathVariable -Name "Path" -Value $installFolderPath -ReplaceMask "$($nvmwPath)*" -Persist "Process";
 
-    if ($Persist -ne '') {
-        # also add to the permanent windows path
-        $persistedPaths = @($requestedVersion)
-        [Environment]::GetEnvironmentVariable('Path', $Persist) -split ';' | % {
-          if (-not($_ -like "$nvmwPath*")) {
-            $persistedPaths += $_
-          }
-        }
-        [Environment]::SetEnvironmentVariable('Path', $persistedPaths -join ';', $Persist)
+    #2. one time for the Persist Mode if it is NOT Process => means to use fo the next started process for that machine/user
+    if ($Persist -ne "Process")
+    {
+      Update-EnvironmentPathVariable -Name "Path" -Value $installFolderPath -ReplaceMask "$($nvmwPath)*" -Persist $Persist;
     }
 
-    $env:NODE_PATH = "$requestedVersion;"
-    npm config set prefix $requestedVersion
+    Update-EnvironmentPathVariable -Name "Path" -Value $installFolderPath -ReplaceMask "$($nvmwPath)*" -Persist $Persist;
+
+    $env:NODE_PATH = "$($installFolderPath);"
+    npm config set prefix $installFolderPath
     $env:NODE_PATH += npm root -g
 
-    "Switched to node version $VersionToUse"
+    Write-Verbose "Switched to node version $($VersionToUse.ToString())"
 }
