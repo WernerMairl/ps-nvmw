6363cb67c93d7ea6478394e4183fe46d10ad616e
Werner Mairl <werner.mairl@gmail.com>
2016-11-12 21:58:57 +0100

    two new (private) methods that are helping a lot in "Set-NodeVersion"

---
+++ nvm/private/Compare-Version.ps1
@@ -0,0 +1,43 @@
+﻿#special Compare for System.Versions where Version.Revision is empty/dot defined and two different default values (0, -1) are used!
+function Compare-Version
+{
+    [cmdletbinding()]
+    param
+    (
+         [Parameter(Mandatory=$true)][System.Version]$A #null not allowed => lower complexity
+        ,[Parameter(Mandatory=$true)][System.Version]$B #null not allowed => lower complexity
+    )
+
+    [int]$result = $A.Major.CompareTo($B.Major);
+    if ($result -ne 0)
+    {
+      return $result;
+    }
+    
+    $result = $A.Minor.CompareTo($B.Minor);
+    if ($result -ne 0)
+    {
+      return $result;
+    }
+
+    $result = $A.Build.CompareTo($B.Build);
+    if ($result -ne 0)
+    {
+      return $result;
+    }
+
+    #special behavior needed for Revision!
+
+    $result = $A.Revision.CompareTo($B.Revision);
+    if ($result -eq 0)
+    {
+      return $result; #they are equal, everything OK
+    }
+    
+    if (   (($A.Revision -eq 0) -or ($A.Revision -eq -1)) -and (($B.Revision -eq 0) -or ($B.Revision -eq -1)) )
+    {
+      $result =0; #revision 0 and -1 means likely the same (not defined with two different defaults used in town)
+    }
+    return $result;
+
+}
---
+++ nvm/private/Update-EnvironmentPathVariable.ps1
@@ -0,0 +1,34 @@
+﻿function Update-EnvironmentPathVariable 
+{
+    [cmdletbinding()]
+    param
+    (
+          [Parameter(Mandatory=$true)][string]$Name
+         ,[Parameter(Mandatory=$true)][string]$Value
+         ,[ValidateSet("Process", "User", "Machine")][Parameter(Mandatory=$false)][string]$Persist #no defaults used on internal/private!
+         ,[Parameter(Mandatory=$false)][string]$ReplaceMask 
+
+    )
+    [System.EnvironmentVariableTarget]$target = $Persist; #Powershell integrated Typecasting system should do the work!
+    Write-Verbose "EnvironemntTarget=$($target)";
+    [string]$currentPath = [Environment]::GetEnvironmentVariable($Name,$target); #empty variable does not cause exception here or later (verified)!!
+
+    [string[]]$parts=@($Value); ##add new value as first!
+    foreach ($item in ($currentPath -split ";"))
+    {
+      if ([string]::IsNullOrEmpty($item))
+      {
+        continue; #don't add this item. this occurs in case where $currentpath is $null (empty/not existing variable)
+      }
+      if ([string]::IsNullOrEmpty($ReplaceMask) -eq $false)
+      {
+          if ($item -like "$($ReplaceMask)*") #basefolder+wildcard means: every active version that is located inside our Get-NodeInstallLocation
+          {
+            continue; #don't add this Item!
+          }
+      }
+      $parts+=$item;
+    }
+    [string]$newPath=$parts -join ";";
+    [Environment]::SetEnvironmentVariable($Name, $newPath, $target); 
+}
