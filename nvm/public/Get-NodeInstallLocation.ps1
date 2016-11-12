e2a35706f3fadb1fe906a66034c16d4a4d85405e
Werner Mairl <werner.mairl@gmail.com>
2016-11-12 22:03:42 +0100

    support for cmdletbinding and -Resolve Parameter

--- nvm/public/Get-NodeInstallLocation.ps1
+++ nvm/public/Get-NodeInstallLocation.ps1
@@ -1,4 +1,5 @@
-﻿function Get-NodeInstallLocation {
+﻿function Get-NodeInstallLocation 
+{
     <#
     .Synopsis
         Gets the currnet node.js install path
@@ -8,14 +9,28 @@
         Get-NodeInstallLocation
         c:\tmp\.nvm
     #>
+  [cmdletbinding()]
+  Param(
+         [Parameter(Mandatory=$false)][Switch]$Resolve = $false
+       )
+
     $settings = $null
-    $settingsFile = Join-Path $PSScriptRoot 'settings.json'
+    $settingsFile = Join-Path $PSScriptRoot "settings.json";
 
-    if ((Test-Path $settingsFile) -eq $true) {
+    if ((Test-Path $settingsFile) -eq $true) 
+    {
         $settings = Get-Content $settingsFile | ConvertFrom-Json
-    } else {
-        $settings = New-Object -TypeName PSObject -Prop @{ InstallPath = (Join-Path $PSScriptRoot 'vs') }
+    } 
+    else 
+    {
+        $settings = New-Object -TypeName PSObject -Prop @{ InstallPath = (Join-Path $PSScriptRoot 'vs') } #We create a CustomObject that implements the same property 'InstallPath" like the then-path-Json!
     }
-
-    $settings.InstallPath
+    if ($Resolve.IsPresent)
+    {
+      return Resolve-Path -Path $settings.InstallPath; #ensures a FULL path that MUST exist!
+    }
+    else
+    {
+      return $settings.InstallPath;
+    }
 }
