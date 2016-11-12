8ba293a34aca5bb14524d03cef04c58c73d1725b
Werner Mairl <werner.mairl@gmail.com>
2016-11-12 10:16:27 +0100

    private function that validates version labels

---
+++ nvm/private/Validate-Version.ps1
@@ -0,0 +1,30 @@
+﻿function Validate-Version
+{
+    param
+    (
+        [string]$Version
+        ,[Switch]$Passthrough = $false
+    )
+    [System.Version]$result=$null;
+    [bool]$success=$false;
+
+    if ([string]::IsNullOrEmpty($Version) -eq $false)
+    {
+      if ($Version.StartsWith("v",$false, [System.Globalization.CultureInfo]::InvariantCulture))
+      {
+         $Version = $Version.Substring(1);
+      }
+      $success = [System.Version]::TryParse($Version,[ref]$result);
+    }
+
+    ## Result Handling
+
+    if ($Passthrough.IsPresent)
+    {
+      return $result;
+    }
+    else
+    {
+      return $success;
+    }
+}
